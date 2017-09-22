0a5b815dde6292fcf7c9cf47cb43ae2a47b8c9ad
Jeferson Machado <jefersonmachado@univille.br>
2017-09-22 19:44:35 -0300

    Update db.js

--- util/db.js
+++ util/db.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 function dbcomm(){
     var connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : '',
-        database : 'dacsnode'
+        host     : 'us-cdbr-sl-dfw-01.cleardb.net',
+        user     : 'bb7bc56af400d0',
+        password : '1d8c08c7',
+        database : 'ibmx_05e550d1e8bc791'
     });
 
     connection.connect(function(err){
